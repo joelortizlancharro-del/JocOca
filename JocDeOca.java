7f9ef7b7437f1a87df4d9245dfd61b1f485766a6
joel <joel.ortizlancharro@gmail.com>
2025-12-04 19:15:13 +0100

    preparat

---
+++ JocDeOca.java
@@ -0,0 +1,10 @@
+public class JocDeOca {
+
+    public static void main(String[] args) {
+        JocDeOca p = new JocDeOca();
+        p.principal();
+    }
+    public void principal(){
+        
+    }
+}
